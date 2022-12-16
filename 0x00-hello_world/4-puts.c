c265e76ffa78a753a9a2bc9ac2cfeda6999ecb71
kamsi05 <victorkamsiyo05@gmail.com>
2022-12-16 02:39:20 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print a given phrase
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multiligual puzzle");
+	return (0)
+} 
