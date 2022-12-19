07f13af950cff8bb529f54cf73dd026a1a01ac96
kamsi05 <victorkamsiyo05@gmail.com>
2022-12-19 11:54:52 -0800

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * _atoi - Convert string to an integer.
@@ -12,7 +12,7 @@ int _atoi(char *s)
 	unsigned int num;
 	char *temp;
 
-	temp = s;
+	temo = s;
 	num = 0;
 	sign = 1;
 	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
