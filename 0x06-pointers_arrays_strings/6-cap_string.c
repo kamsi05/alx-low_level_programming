67d260022705dd3cee3854d2a224cf1df86ea73a
kamsi05 <victorkamsiyo05@gmail.com>
2022-12-22 12:31:25 -0800

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes ll words of a string.
+ * @str: The string to be capitalized.
+ * Return: A pointer to the changed string.
+ */
+
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while ([index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= '2'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+			str[index] -= 32;
+		index++;
+	}
+
+	return (str);
+}
